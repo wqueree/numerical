a3f6bfd38acbebe92da0b004f1b5a3a53466d9d6
wqueree <wqueree@gmail.com>
2025-02-05 19:53:43 +0000

    Tidy up templates and invalidate caches on entry mutation

--- Matrix.hpp
+++ Matrix.hpp
@@ -27,8 +27,30 @@ private:
     mutable std::array<std::size_t, M> permutation_;
 
 
+    template <std::size_t _M, std::size_t _N>
+    struct is_square : std::integral_constant<bool, _M == _N> {};
+
+
+    template <std::size_t _M, std::size_t _N, typename _T>
+    using enable_if_square = std::enable_if<is_square<_M, _N>::value, _T>;
+
+
+    template <std::size_t _M, std::size_t _N, typename _T>
+    using enable_if_square_v = typename enable_if_square<_M, _N, _T>::value;
+
+
+    template <std::size_t _M, std::size_t _N, typename _T>
+    using enable_if_square_t = typename enable_if_square<_M, _N, _T>::type;
+
+
+    void invalidate_caches() {
+        determinant_cache_valid_ = false;
+        lup_decomposition_cache_valid_ = false;
+    }
+
+
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N), void>::type lup_decompose() const {
+    constexpr typename enable_if_square<_M, _N, void>::type lup_decompose() const {
         Matrix<T, M, N> A {*this};
         for (std::size_t k = 0; k < M; ++k)
         {
@@ -49,31 +71,19 @@ private:
             {
                 A(i, k) /= A(k, k);
                 for (std::size_t j = k + 1; j < M; ++j)
-                {
                     A(i, j) -= A(i, k) * A(k, j);
-                }
             }
 
             lower_ = std::make_shared<Matrix<T, M, N>>(std::move(Matrix<T, M, N>::identity()));
             for (std::size_t i = 1; i < M; ++i)
-            {
                 for (std::size_t j = 0; j < i; ++j)
-                {
                     (*lower_)(i, j) = A(i, j);
-                }
-            }
 
             upper_ = std::make_shared<Matrix<T, M, N>>();
             for (std::size_t i = 0; i < M; ++i)
-            {
                 for (std::size_t j = i; j < N; ++j)
-                {
                     (*upper_)(i, j) = A(i, j);
-                }
-            }
         }
-
-
         lup_decomposition_cache_valid_ = true;
     }
 
@@ -123,7 +133,7 @@ public:
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    static typename std::enable_if<_M == _N, Matrix<_T, _M, _N>>::type identity() {
+    static typename enable_if_square<_M, _N, Matrix<_T, _M, _N>>::type identity() {
         Matrix<_T, _M, _N> _identity;
         for (size_t i = 0; i < _M; ++i)
             _identity(i, i) = _T(1);
@@ -135,6 +145,7 @@ public:
         if (row >= M || col >= N) {
             throw std::out_of_range("Matrix indices out of bounds");
         }
+        invalidate_caches();
         return elements_[row][col];
     }
 
@@ -159,7 +170,7 @@ public:
 
 
     Matrix<T, M, N> operator+(const Matrix<T, M, N>& other) {
-        Matrix<T, M, N> sum {};
+        Matrix<T, M, N> sum;
         for (size_t i = 0; i < M; ++i) {
             for (size_t j = 0; j < N; ++j) {
                 sum(i, j) = (*this)(i, j) + other(i, j);
@@ -168,10 +179,8 @@ public:
         return sum;
     }
 
-
-    constexpr Matrix<T, M, N> operator-(const Matrix<T, M, N>& other) {
+    Matrix<T, M, N> operator-(const Matrix<T, M, N>& other) {
         Matrix<T, M, N> sum;
-        std::array<std::array<T, N>, M> sum_array {};
         for (size_t i = 0; i < M; ++i) {
             for (size_t j = 0; j < N; ++j) {
                 sum(i, j) = (*this)(i, j) - other(i, j);
@@ -179,7 +188,6 @@ public:
         }
         return sum;
     }
-
 
     template <std::size_t P>
     constexpr Matrix<T, M, P> operator*(const Matrix<T, N, P>& other) {
@@ -198,19 +206,19 @@ public:
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N) && (_M == 1), _T>::type determinant() {
+    constexpr typename std::enable_if<is_square<_M, _N>::value && _M == 1, _T>::type determinant() {
         return (*this)(0, 0);
     }
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N) && (_M == 2), _T>::type determinant() {
+    constexpr typename std::enable_if<is_square<_M, _N>::value && _M == 2, _T>::type determinant() {
         return (*this)(0, 0) * (*this)(1, 1) - (*this)(0, 1) * (*this)(1, 0);
     }
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N) && (_M > 2), _T>::type determinant() {
+    constexpr typename std::enable_if<is_square<_M, _N>::value && (_M > 2), _T>::type determinant() {
         if (determinant_cache_valid_) return determinant_cache_;
 
         _T _determinant = 0;
@@ -237,45 +245,43 @@ public:
 
     constexpr Matrix<T, N, M> transpose() const {
         Matrix<T, N, M> _transpose;
-        for (size_t i = 0; i < M; ++i) {
-            for (size_t j = 0; j < N; ++j) {
+        for (size_t i = 0; i < M; ++i)
+            for (size_t j = 0; j < N; ++j)
                 _transpose(j, i) = (*this)(i, j);
-            }
-        }
         return _transpose;
     }
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N), std::shared_ptr<Matrix<T, M, N>>>::type lower() const {
+    constexpr typename enable_if_square<_M, _N, std::shared_ptr<Matrix<T, M, N>>>::type lower() const {
         if (!lup_decomposition_cache_valid_) lup_decompose();
         return lower_;
     }
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N), std::shared_ptr<Matrix<T, M, N>>>::type upper() const {
+    constexpr typename enable_if_square<_M, _N, std::shared_ptr<Matrix<T, M, N>>>::type upper() const {
         if (!lup_decomposition_cache_valid_) lup_decompose();
         return upper_;
     }
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N), std::array<std::size_t, M>>::type permutation() const {
+    constexpr typename enable_if_square<_M, _N, std::array<std::size_t, M>>::type permutation() const {
         if (!lup_decomposition_cache_valid_) lup_decompose();
         return permutation_;
     }
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N), std::tuple<std::shared_ptr<Matrix<T, M, N>>, std::shared_ptr<Matrix<T, M, N>>, std::array<std::size_t, M>>> lup_decomposition() const {
+    constexpr typename enable_if_square<_M, _N, std::tuple<std::shared_ptr<Matrix<T, M, N>>, std::shared_ptr<Matrix<T, M, N>>, std::array<std::size_t, M>>>::type lup_decomposition() const {
         if (!lup_decomposition_cache_valid_) lup_decompose();
         return {lower_, upper_, permutation_};
     }
 
 
     template <typename _T = T, std::size_t _M = M, std::size_t _N = N>
-    constexpr typename std::enable_if<(_M == _N), Matrix<_T, _M, 1>>::type lup_solve(Matrix<_T, _M, 1>& b) const {
+    constexpr typename enable_if_square<_M, _N, Matrix<_T, _M, 1>>::type lup_solve(Matrix<_T, _M, 1>& b) const {
         if (!lup_decomposition_cache_valid_) lup_decompose();
         Matrix<_T, _M, 1> x;
         Matrix<_T, _M, 1> y;
@@ -292,9 +298,7 @@ public:
         {
             x(i, 0) = y(i, 0);
             for (std::size_t j = i + 1; j < _N; ++j)
-            {
                 x(i, 0) -= (*upper_)(i, j) * x(j, 0);
-            }
             x(i, 0) /= (*upper_)(i, i);
         }
 
@@ -310,6 +314,7 @@ public:
             next = permutation[next];
             ++swaps;
         }
+        invalidate_caches();
     }
 
 
